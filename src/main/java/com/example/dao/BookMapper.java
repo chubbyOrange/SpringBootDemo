5322d8c9f7310d930c05ef826dd48864d8012430
Mason Jing <y38jing@uwaterloo.ca>
2022-03-22 19:42:29 +0800

    Uncomment Out BookMapper

--- src/main/java/com/example/dao/BookMapper.java
+++ src/main/java/com/example/dao/BookMapper.java
@@ -4,10 +4,10 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.domain.Book;
 
 import org.apache.ibatis.annotations.Mapper;
-// import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface BookMapper extends BaseMapper<Book> {
-    // @Select("select * from books where bid=#{id}")
-    // public Books getById(Integer id);
+    @Select("select * from books where bid=#{id}")
+    public Book getById(Integer id);
 }
